9a8ed101f18b5785edf8c21cc10755feb4488355
tomoki_sun <tomoki69386@gmail.com>
2020-10-25 00:41:16 +0900

    Fix test

--- AppTests/InfoTests.swift
+++ AppTests/InfoTests.swift
@@ -8,18 +8,8 @@
 import XCTest
 
 class InfoTests: XCTestCase {
-    func testAdHoc() {
-        let path = Bundle.main.path(forResource: "AdHoc", ofType: "plist")
-        XCTAssertNotNil(path)
-    }
-    
-    func testDebug() {
-        let path = Bundle.main.path(forResource: "Debug", ofType: "plist")
-        XCTAssertNotNil(path)
-    }
-    
-    func testRelease() {
-        let path = Bundle.main.path(forResource: "Release", ofType: "plist")
+    func testInfoPlist() {
+        let path = Bundle.main.path(forResource: "Info", ofType: "plist")
         XCTAssertNotNil(path)
     }
 }
